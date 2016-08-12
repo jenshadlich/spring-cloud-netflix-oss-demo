87ef426e71349285c36799e0decd73a1d71383e6
jenshadlich <jenshadlich@googlemail.com>
2016-08-12 15:56:38 +0200

    refactor

--- discovery/src/main/java/de/jeha/demo/spring/cloud/netflix/oss/discovery/EurekaServerApplication.java
+++ discovery/src/main/java/de/jeha/demo/spring/cloud/netflix/oss/discovery/EurekaServerApplication.java
@@ -1,18 +1,18 @@
 package de.jeha.demo.spring.cloud.netflix.oss.discovery;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author jenshadlich@googlemail.com
  */
-@EnableEurekaServer
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
     public static void main(String... args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        new SpringApplicationBuilder(EurekaServerApplication.class).web(true).run(args);
     }
 
 }
