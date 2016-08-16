f4e74a8e321adadda0b33037b2dcc16eb10ced8e
jenshadlich <jenshadlich@googlemail.com>
2016-08-16 16:41:58 +0200

    add turbine

---
+++ monitoring/src/main/java/de/jeha/demo/spring/cloud/netflix/oss/monitoring/TurbineServerApplication.java
@@ -0,0 +1,20 @@
+package de.jeha.demo.spring.cloud.netflix.oss.monitoring;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+@SpringBootApplication
+@EnableTurbine
+@EnableDiscoveryClient
+public class TurbineServerApplication {
+
+    public static void main(String... args) {
+        new SpringApplicationBuilder(TurbineServerApplication.class).web(true).run(args);
+    }
+
+}
