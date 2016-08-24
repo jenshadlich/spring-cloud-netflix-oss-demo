97e92d64e144299d5b1743f841244de320c1d4db
jenshadlich <jenshadlich@googlemail.com>
2016-08-24 12:43:45 +0200

    add user resource

--- security/src/main/java/de/jeha/demo/spring/cloud/netflix/oss/security/AuthServerApplication.java
+++ security/src/main/java/de/jeha/demo/spring/cloud/netflix/oss/security/AuthServerApplication.java
@@ -4,19 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 /**
  * @author jenshadlich@googlemail.com
  */
 @SpringBootApplication
 @RestController
-//@EnableResourceServer
+@EnableResourceServer
 @EnableAuthorizationServer
 public class AuthServerApplication {
 
     public static void main(String... args) {
         SpringApplication.run(AuthServerApplication.class, args);
+    }
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
     }
 
 }
