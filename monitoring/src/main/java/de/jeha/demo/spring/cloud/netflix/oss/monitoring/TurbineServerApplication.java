9b95b952081f1c7ffd31d952c62e39d5f84e52de
jenshadlich <jenshadlich@googlemail.com>
2016-08-24 10:53:08 +0200

    add hystrix dashboard

--- monitoring/src/main/java/de/jeha/demo/spring/cloud/netflix/oss/monitoring/TurbineServerApplication.java
+++ monitoring/src/main/java/de/jeha/demo/spring/cloud/netflix/oss/monitoring/TurbineServerApplication.java
@@ -3,6 +3,7 @@ package de.jeha.demo.spring.cloud.netflix.oss.monitoring;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
 @SpringBootApplication
 @EnableTurbine
 @EnableDiscoveryClient
+@EnableHystrixDashboard
 public class TurbineServerApplication {
 
     public static void main(String... args) {
