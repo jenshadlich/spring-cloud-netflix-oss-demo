54c91702a8db9a8a73c2d2b4a1c5524184734c44
jenshadlich <jenshadlich@googlemail.com>
2016-08-24 09:13:06 +0200

    disable OAuth2

--- compound/product/src/main/java/de/jeha/spring/cloud/netlix/oss/compound/product/ProductCompoundApplication.java
+++ compound/product/src/main/java/de/jeha/spring/cloud/netlix/oss/compound/product/ProductCompoundApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
  * @author jenshadlich@googlemail.com
