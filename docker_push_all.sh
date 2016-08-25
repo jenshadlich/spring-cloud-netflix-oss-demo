388b424a2c07bae5de2e9336ffab857a7f3a69ad
jenshadlich <jenshadlich@googlemail.com>
2016-08-25 13:27:27 +0200

    add

---
+++ docker_push_all.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+docker push jenshadlich/sc-compound-product:latest
+docker push jenshadlich/sc-core-product:latest
+docker push jenshadlich/sc-core-stock:latest
+docker push jenshadlich/sc-eureka-server:latest
+docker push jenshadlich/sc-zuul-server:latest
+docker push jenshadlich/sc-turbine-server:latest
+docker push jenshadlich/sc-auth-server:latest
